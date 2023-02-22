981c314736fca76be2a5256324a81b64cc8dec63
Michael Kelly <mikelly1988@gmail.com>
2023-02-22 14:12:11 +0000

    updating snippet

---
+++ app/src/main/java/com/example/placemark/activities/activities/MapActivity.kt
@@ -0,0 +1,99 @@
+package com.example.placemark.activities.activities
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.drawable.BitmapDrawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.content.ContextCompat
+import com.example.placemark.R
+import com.example.placemark.activities.models.Location
+import com.example.placemark.databinding.ActivityMapBinding
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.*
+
+class MapActivity : AppCompatActivity(), OnMapReadyCallback,
+                                        GoogleMap.OnMarkerDragListener,
+                                        GoogleMap.OnMarkerClickListener {
+
+    private lateinit var map: GoogleMap
+    private lateinit var binding: ActivityMapBinding
+    private var location = Location()
+    var listOfMarkers : ArrayList<Marker> ? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMapBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        location = intent.extras?.getParcelable<Location>("location")!!
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        val loc = LatLng(location.lat, location.lng)
+        val options = MarkerOptions()
+            .title("Placemark")
+            .snippet("GPS : $loc")
+            .draggable(true)
+            .position(loc)
+            .icon(bitmapDescriptorFromVector(this, R.drawable.baseline_directions_run_24))
+        map.addMarker(options)
+        map.setOnMarkerDragListener(this)
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(loc, location.zoom))
+    }
+
+    override fun onMarkerDrag(marker: Marker) {
+        if(onMarkerClick(marker)) {
+            onMarkerDragStart(marker)
+        }
+    }
+
+    private fun bitmapDescriptorFromVector(context: Context, vectorResId: Int): BitmapDescriptor? {
+        return ContextCompat.getDrawable(context, vectorResId)?.run {
+            setBounds(0, 0, intrinsicWidth, intrinsicHeight)
+            val bitmap = Bitmap.createBitmap(intrinsicWidth, intrinsicHeight, Bitmap.Config.ARGB_8888)
+            draw(Canvas(bitmap))
+            BitmapDescriptorFactory.fromBitmap(bitmap)
+        }
+    }
+
+    override fun onMarkerDragEnd(marker: Marker) {
+        location.lat = marker.position.latitude
+        location.lng = marker.position.longitude
+        location.zoom = map.cameraPosition.zoom
+
+    }
+
+    override fun onMarkerDragStart(marker: Marker) {
+        location.lat += marker.position.latitude
+        location.lng += marker.position.longitude
+        location.zoom = map.cameraPosition.zoom
+
+    }
+
+    override fun onMarkerClick(marker: Marker): Boolean {
+        val loc = LatLng(location.lat, location.lng)
+        marker.snippet = "GPS : $loc"
+        return true
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onBackPressed() {
+        val resultIntent = Intent()
+        resultIntent.putExtra("location", location)
+        setResult(Activity.RESULT_OK, resultIntent)
+        finish()
+        super.onBackPressed()
+    }
+
+}
